ecd9813d486b5583cf4ac9119e45f2ca85228f75
Amanda_Katherine <walker.akw@gmail.com>
2020-07-18 17:20:19 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word        
+    end
+
+    def match(string)
+        matching_cases = []
+        string.find_all do |array_word|
+            if word.split("").sort == array_word.split("").sort
+                matching_cases << array_word
+            end
+        end
+        matching_cases
+    end
+end
