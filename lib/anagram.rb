fa19e2d79795fe430e4d47ad48dbde25e484837e
Amanda_Katherine <walker.akw@gmail.com>
2020-07-18 17:32:20 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,11 +6,8 @@ class Anagram
     end
 
     def match(string)
-       # matching_cases = []
         string.find_all do |array_word|
             word.split("").sort == array_word.split("").sort
-              #  matching_cases << array_word
         end
-        #matching_cases
     end
 end
